9ea738ef96336c65623574e676b7fe36431c234e
Berfim Korkmaz <58472240+Berfimm@users.noreply.github.com>
2023-08-10 10:22:51 +0300

    Update RDWorklogTracking.cs

--- W2UIFullCalendarProject/Models/RDWorklogTracking.cs
+++ W2UIFullCalendarProject/Models/RDWorklogTracking.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TCPortal.Models
+namespace W2UIFullCalendarProject.Models
 {
     public class RDWorklogTracking
     {
